d0ef50edbc17953c36b3b57a6f18114d8207924e
Keith Kovach <kovachks90@gmail.com>
2019-01-26 00:42:09 -0500

    removed port

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@
 
 // Enabling Websocket
 if (window.location.hostname !== 'localhost') {
-    var socket = io.connect('https://boiling-beyond-83726.herokuapp.com/:80')
+    var socket = io.connect('https://boiling-beyond-83726.herokuapp.com/')
 } else {
     var socket = io.connect('http://' + window.location.hostname);
 }

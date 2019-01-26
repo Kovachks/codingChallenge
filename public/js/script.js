958bb84d1739bffa1442fbae6b0f53fbdd421f29
Keith Kovach <kovachks90@gmail.com>
2019-01-26 01:30:00 -0500

    updated

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,9 @@
 var socket = io();
 var el = document.getElementById('server-time');
 socket.on('time', function(timeString) {
+
+    console.log('fired')
+
   el.innerHTML = 'Server time: ' + timeString;
 });
 
